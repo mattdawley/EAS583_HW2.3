ed2bac2c163d3461f9fd0eec851febcb2280aab9
Matt Dawley <dawley@gmail.com>
2024-01-22 20:06:09 -0700

    first attempt

--- bayc_supply.py
+++ bayc_supply.py
@@ -1,17 +1,19 @@
 from web3 import Web3
+from web3 import utils
 from web3.contract import Contract
 from web3.providers.rpc import HTTPProvider
+import json
 import requests
 
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-contract_address = Web3.toChecksumAddress(bayc_address)
+contract_address = Web3.to_checksum_address(bayc_address)
 
 ############################
 #Get Contract ABI from etherscan
 ABI_ENDPOINT = 'https://api.etherscan.io/api?module=contract&action=getabi&address='
 try:
 	response = requests.get( f"{ABI_ENDPOINT}{contract_address}", timeout = 20 )	
-	abi = response.json()
+	abi = response.json()["result"]
 except Exception as e:
 	print( f"Failed to get {contract_address} from {ABI_ENDPOINT}" )
 	print( e )
@@ -20,12 +22,14 @@ bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 
 ############################
 #Connect to an Ethereum node
-token = "Mwb3juVAfI1g2RmA1JCGdYk-2_BmFrnLOtbomP1oDa4"
-api_url = f"https://c2emjgrvmi7cabd41mpg.bdnodes.net?auth={token}"
+#token = "Mwb3juVAfI1g2RmA1JCGdYk-2_BmFrnLOtbomP1oDa4"
+#api_url = f"https://c2emjgrvmi7cabd41mpg.bdnodes.net?auth={token}"
+api_url = "https://mainnet.infura.io/v3/7d971d5755d142b49329f02a8bfd5d72"
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
-contract = web3.eth.contract(address=contract_address,abi=abi)
+contract = web3.eth.contract(address=bayc_address, abi=abi)
 supply = contract.functions.totalSupply().call()
 
 print( f"Supply = {supply}" )
+print(contract)
--- get_ape_info.py
+++ get_ape_info.py
@@ -1,4 +1,4 @@
-from web3 import Web3
+from web3 import Web3, contract
 from web3.contract import Contract
 from web3.providers.rpc import HTTPProvider
 import requests
@@ -6,18 +6,19 @@ import json
 import time
 
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-contract_address = Web3.toChecksumAddress(bayc_address)
+contract_address = Web3.to_checksum_address(bayc_address)
 
 #You will need the ABI to connect to the contract
 #The file 'abi.json' has the ABI for the bored ape contract
 #In general, you can get contract ABIs from etherscan
 #https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
-with open('/home/codio/workspace/abi.json', 'r') as f:
-	abi = json.load(f) 
+#with open('/home/codio/workspace/abi.json', 'r') as f:
+with open('abi.json','r') as f:
+	abi = json.load(f)
 
 ############################
 #Connect to an Ethereum node
-api_url = #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
+api_url = "https://mainnet.infura.io/v3/7d971d5755d142b49329f02a8bfd5d72"
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
@@ -25,11 +26,24 @@ def get_ape_info(apeID):
 	assert isinstance(apeID,int), f"{apeID} is not an int"
 	assert 1 <= apeID, f"{apeID} must be at least 1"
 
-	data = {'owner': "", 'image': "", 'eyes': "" }
-	
-	#YOUR CODE HERE	
+	contract = web3.eth.contract(address=bayc_address, abi=abi)
+	result = contract.functions.tokenURI(apeID).call()
+	owner = contract.functions.ownerOf(apeID).call()
+	uri = result.replace("ipfs://","")
+	base_url = "https://gateway.pinata.cloud/ipfs/"
+	pinata_gateway_url = base_url+uri
+	headers = {"content_type": "json"}
+	response = requests.get(pinata_gateway_url, headers)
+	j_data = response.json()
+	data = {'owner': owner, 'image': j_data["image"], 'eyes': j_data["attributes"][3]["value"] }
 
 	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
 	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
 	return data
 
+"""Test
+print("Ape # 1", get_ape_info(1))
+print("-----")
+for i in range(100, 110):
+	print("Ape #",i,get_ape_info(i))
+"""
